2fe12d066b1c31df302aa816be703af6195b8102
kyj9515 <ydh981@naver.com>
2023-07-06 12:01:14 +0900

    vt100 clear

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,9 @@
+public class Hello { 
+	public static void main(String[] args) {
+	
+		System.out.println("\033[2J");
 
-class HelloWorld {
-	public static void main(String[] args) { 
-		System.out.println("Hello"); 
- }
-  
+		System.out.println("Hello Java");
+	
+	}
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,6 +1,7 @@
 class Triangle {
 
 	public static void main(String[] args) {
+		// 삼각형 출력
 		System.out.println(" *****");
 		System.out.println("  ****");
 		System.out.println("   ***");
