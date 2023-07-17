c3b71b9b2c936eadbc8512e86bc4f0177dd8efb9
kyj9515 <ydh981@naver.com>
2023-07-17 18:06:41 +0900

    ...

---
+++ src/comment.java
@@ -0,0 +1,27 @@
+
+/**
+ * 주석처리 방법 연구 <br>
+ * 1.한줄 주석 기호// <br>
+ * 2.여러줄 주석 기호 <br>
+ * 3.자바덕 주석 기호 <br>
+ */
+
+
+
+public class comment {
+
+	//한줄삭제 Ctrl + d 
+	//주석토글 Ctrl + /
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Hi");            // print는 new line을 삽입하지 않는다.
+		System.out.println("Hello Java!");
+		System.out.print("Hi");
+		
+		
+	}
+
+}
